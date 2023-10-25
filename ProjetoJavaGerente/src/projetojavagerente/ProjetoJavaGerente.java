bb343684412e928fb3b5c58fdba154924b693cbd
Iago Rosa de Oliveira <125699322+iagorosa28@users.noreply.github.com>
2023-10-25 16:40:14 -0300

    começo da main do gerente

---
+++ ProjetoJavaGerente/src/projetojavagerente/ProjetoJavaGerente.java
@@ -0,0 +1,15 @@
+
+package projetojavagerente;
+
+import view.SenhaBanco;
+
+public class ProjetoJavaGerente {
+
+    public static void main(String[] args) {
+
+        SenhaBanco sb = new SenhaBanco();
+        sb.setVisible(true);
+        
+    }
+    
+}
